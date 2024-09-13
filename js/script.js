c2de72352c4ee43a48fc054fdd9ecb2f4904a9bb
nparth29 <parthjnv123@gmail.com>
2024-09-13 20:59:29 +0530

    register page added
    i have added student and teacher register pages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 document.getElementById('studentLoginBtn').addEventListener('click', function() {
-    document.getElementById('studentLoginForm').style.display = 'block';
-    document.getElementById('teacherLoginForm').style.display = 'none';
-  });
-  
-  document.getElementById('teacherLoginBtn').addEventListener('click', function() {
-    document.getElementById('teacherLoginForm').style.display = 'block';
-    document.getElementById('studentLoginForm').style.display = 'none';
-  });
-  
+  document.getElementById('studentLoginForm').style.display = 'block';
+  document.getElementById('teacherLoginForm').style.display = 'none';
+});
+
+document.getElementById('teacherLoginBtn').addEventListener('click', function() {
+  document.getElementById('teacherLoginForm').style.display = 'block';
+  document.getElementById('studentLoginForm').style.display = 'none';
+});
+
+document.getElementById('studentRegisterBtn').addEventListener('click', function() {
+  window.location.href = 'student-register.html';
+});
+
+document.getElementById('teacherRegisterBtn').addEventListener('click', function() {
+  window.location.href = 'teacher-register.html';
+});
